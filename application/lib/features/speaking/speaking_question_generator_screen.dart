0c24228f8a090327279d8df5a5a656452206c02b
T. Kudo <137812451+hirokd0627@users.noreply.github.com>
2026-02-15 23:58:18 +0900

    Refactoring (#36)
    * Remove question list from speaking question generator screen
    * Adjust logging logic

--- application/lib/features/speaking/speaking_question_generator_screen.dart
+++ application/lib/features/speaking/speaking_question_generator_screen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ielts_ai_trainer/app/theme/app_styles.dart';
 import 'package:ielts_ai_trainer/features/speaking/speaking_question_generator_form.dart';
-import 'package:ielts_ai_trainer/shared/database/app_database.dart';
 import 'package:ielts_ai_trainer/shared/enums/test_task.dart';
-import 'package:ielts_ai_trainer/shared/question_list/question_list_controller.dart';
-import 'package:ielts_ai_trainer/shared/question_list/question_list_query_service.dart';
-import 'package:ielts_ai_trainer/shared/question_list/question_list_view.dart';
 import 'package:ielts_ai_trainer/shared/views/texts.dart';
-import 'package:provider/provider.dart';
 
 /// Question Generator Screen for Part 1, Part 2, and Part 3.
 class SpeakingQuestionGeneratorScreen extends StatefulWidget {
@@ -44,9 +39,6 @@ class SpeakingQuestionGeneratorScreen extends StatefulWidget {
 /// State for SpeakingQuestionGeneratorScreen
 class _SpeakingQuestionGeneratorScreenState
     extends State<SpeakingQuestionGeneratorScreen> {
-  /// Controller for QuestionListView
-  late final QuestionListController _questionListController;
-
   /// Returns the screen title based on the task type.
   String get _screenTitle {
     if (widget.testTask == TestTask.speakingPart1) {}
@@ -55,23 +47,6 @@ class _SpeakingQuestionGeneratorScreenState
         : widget.testTask == TestTask.speakingPart2
         ? 'Speaking Part 2'
         : 'Speaking Part 3';
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _questionListController = QuestionListController(
-      queryService: QuestionListQueryService(context.read<AppDatabase>()),
-      testTask: widget.testTask,
-    );
-
-    _loadInitialData();
-  }
-
-  /// Loads the initial data.
-  Future<void> _loadInitialData() async {
-    await _questionListController.refreshListRows();
   }
 
   @override
@@ -98,29 +73,6 @@ class _SpeakingQuestionGeneratorScreenState
                 topics: widget.topics,
               ),
               SizedBox(height: 40),
-            ],
-          ),
-        ),
-        // Question List
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Header
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: AppStyles.screenPadding,
-                ),
-                child: HeadlineText("Solved Questions"),
-              ),
-              SizedBox(height: 20),
-              // Question list
-              Expanded(
-                child: QuestionListView(
-                  searchBarEnabled: false,
-                  controller: _questionListController,
-                ),
-              ),
             ],
           ),
         ),
